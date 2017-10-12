d67d35ca31cee5cd8710cc90dde6aac4892916c6
Wells <chawkeye@gmail.com>
2017-10-12 14:43:26 +0800

    Added over due_date function

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -26,13 +26,19 @@ class TodosController < ApplicationController
   end
 
   def destroy
-    @todo.destroy
-    redirect_to todos_url
+    compare_date = @todo.due_date <=> Time.now.to_date
+    if compare_date <= 0
+      redirect_to todos_url, notice: "超過完成日期無法刪除！"
+    else
+      @todo.destroy
+      redirect_to todos_url, notice: "待辦事項已刪除！"
+    end
+
   end
 
   def complete
     @todo.update_attribute(:completed_at, Time.now)
-    redirect_to todos_url, notice: "Todo is complete"
+    redirect_to todos_url, notice: "待辦事項完成！"
   end
 
   private
